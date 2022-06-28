8bc53641634252f009a226aada96df1b12077cb7
jwardoin <jordanwardoin@gmail.com>
2022-06-27 20:51:28 -0500

    updated allowed settings

--- services/user.js
+++ services/user.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 
 module.exports = {
     changeSetting: async (googleId, settingName, settingValue) => {
-        const allowedSettings = ['firstName', 'lastName', 'hourlyRate', 'lessonCalendarId']
+        const allowedSettings = ['displayName', 'hourlyRate', 'lessonCalendarId']
         try {
             if (!allowedSettings.includes(settingName)) throw new Error('Not an allowed setting')
             return User.findOneAndUpdate({ googleId }, { [settingName]: settingValue})
