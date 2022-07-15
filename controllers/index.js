2c6cf33fef6e5159626000f7bc1e1659cb3a5ca3
jwardoin <jordanwardoin@gmail.com>
2022-07-14 19:26:58 -0500

    formatted order of data from DB

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const Invoice = require('../models/Invoice')
 
 module.exports = {
     getDashboard: async (req,res) => {
-        const invoices = await Invoice.find({ googleId: req.user.googleId })
+        const invoices = await Invoice.find({ googleId: req.user.googleId }).sort({startDate: -1})
         try {  
             res.json({
                 invoices: invoices
